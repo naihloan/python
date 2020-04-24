cce2f77e7f4b3db3116ea36179cef3d8e6c91e2c
naihloan <benjaminjuarezarlt@gmail.com>
2020-04-24 14:59:15 -0300

    ideas para proyectos

---
+++ mwe/97-ideas-proyectos.py
@@ -0,0 +1,6 @@
+Aplicación con archivos de restaurant
+Directorio de Empresa con subcarpetas /gerencia /ventas
+Crear un juego de Piedra, Papel o Tijeras
+Sistema de administración de biblioteca con préstamo de libros: un libro prestado deberá moverse de carpeta hacia prestados
+
+Comienza en pequeño. Piensa en grande!
